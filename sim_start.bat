87574bd5bc783894ce0124fb05e82a4b0cc31526
xueayi <xueayi503@qq.com>
2023-12-10 03:38:11 +0800

    脚本更新version1.1

--- sim_start.bat
+++ sim_start.bat
@@ -1,4 +1,5 @@
 rd /s /q simulation		%删除仿真文件夹%
+del *.vcd *.out *.txt	%删除忘记加代码生成的文件%
 md simulation		%重建仿真文件夹%
 iverilog -o "wave.out" *_rtl.v *_tb.v    %编译rtl和tb生成中间文件%
 vvp wave.out     %用中间文件进行仿真，根据tb的dump指令生成波形文件vcd%
