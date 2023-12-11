ca73ffaa9c41ff3da9c1684e226c5a9d481cc0e8
xueayi <xueayi503@qq.com>
2023-12-11 21:14:29 +0800

    暂停

--- sim_start.bat
+++ sim_start.bat
@@ -1,8 +1,9 @@
-rd /s /q simulation		%删除仿真文件夹%
-del *.vcd *.out *.txt	%删除忘记加代码生成的文件%
-md simulation		%重建仿真文件夹%
-iverilog -o "wave.out" *_rtl.v *_tb.v    %编译rtl和tb生成中间文件%
-vvp wave.out     %用中间文件进行仿真，根据tb的dump指令生成波形文件vcd%
-move wave.out ./simulation		%移动中间文件到仿真文件夹%
-gtkwave ./simulation/wave.vcd     %打开波形文件%
+rd /s /q simulation		%删锟斤拷锟斤拷锟斤拷锟侥硷拷锟斤拷%
+del *.vcd *.out *.txt	%删锟斤拷锟斤拷锟角加达拷锟斤拷锟斤拷锟缴碉拷锟侥硷拷%
+md simulation		%锟截斤拷锟斤拷锟斤拷锟侥硷拷锟斤拷%
+iverilog -o "wave.out" *_rtl.v *_tb.v    %锟斤拷锟斤拷rtl锟斤拷tb锟斤拷锟斤拷锟叫硷拷锟侥硷拷%
+vvp wave.out     %锟斤拷锟叫硷拷锟侥硷拷锟斤拷锟叫凤拷锟芥，锟斤拷锟斤拷tb锟斤拷dump指锟斤拷锟斤拷锟缴诧拷锟斤拷锟侥硷拷vcd%
+move wave.out ./simulation		%锟狡讹拷锟叫硷拷锟侥硷拷锟斤拷锟斤拷锟斤拷锟侥硷拷锟斤拷%
+gtkwave ./simulation/wave.vcd     %锟津开诧拷锟斤拷锟侥硷拷%
+pause
 %version1.3%
